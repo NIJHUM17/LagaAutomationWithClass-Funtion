67741bef33dfb587a7e414d560dcc575ee46bead
NIJHUM17 <tahsinanijum5199@gmail.com>
2022-10-31 11:04:21 +0600

    Reg

--- LagaAutomation.py
+++ LagaAutomation.py
@@ -14,14 +14,15 @@ class LagaAutomation:
         print(self.driver.title)
         time.sleep(3)
 
+''' 
     def Reg(self):
         self.driver.find_element("xpath", "//a[@class='login']").click()
 
     def email(self, myemail):
         self.driver.find_element("id", "email_create").send_keys(myemail)
         time.sleep(2)
-        driver.find_element("xpath", "//button[@id='SubmitCreate']").click()
-        time.sleep(2)
+        self.driver.find_element("xpath", "//button[@id='SubmitCreate']").click()
+        time.sleep(7)
 
     def name(self, firstname, lastname, password):
         self.driver.find_element("xpath", "//input[@id='id_gender2']").click()
@@ -38,27 +39,30 @@ class LagaAutomation:
         self.driver.find_element("xpath", "//input[@id='optin']").click()
 
     def personal_info(self, firstname, lastname, company, address, addressLine2, city, state, zip_code, others ,Home_phone, mobile):
-        driver.find_element("id", "firstname").send_keys(firstname)
-        driver.find_element("id", "lastname").send_keys(lastname)
-        driver.find_element("id", "company").send_keys(company)
-        driver.find_element("id", "address1").send_keys(address)
-        driver.find_element("id", "address2").send_keys(addressLine2)
-        driver.find_element("id", "city").send_keys(city)
-        driver.find_element("id", "id_state").send_keys(state)
-        driver.find_element("id", "postcode").send_keys(zip_code)
-        driver.find_element("id", "other").send_keys(others)
-        driver.find_element("id", "phone").send_keys(Home_phone)
-        driver.find_element("id", "phone_mobile").send_keys(mobile)
-        driver.find_element("id", "alias").send_keys(address)
+        self.driver.find_element("id", "firstname").send_keys(firstname)
+        self.driver.find_element("id", "lastname").send_keys(lastname)
+        self.driver.find_element("id", "company").send_keys(company)
+        self.driver.find_element("id", "address1").send_keys(address)
+        self.driver.find_element("id", "address2").send_keys(addressLine2)
+        self.driver.find_element("id", "city").send_keys(city)
+        self.driver.find_element("id", "id_state").send_keys(state)
+        self.driver.find_element("id", "postcode").send_keys(zip_code)
+        self.driver.find_element("id", "other").send_keys(others)
+        self.driver.find_element("id", "phone").send_keys(Home_phone)
+        self.driver.find_element("id", "phone_mobile").send_keys(mobile)
+        self.driver.find_element("id", "alias").send_keys(address)
 
-        driver.find_element("xpath", "//button[@id='submitAccount']").click()
+        self.driver.find_element("xpath", "//button[@id='submitAccount']").click()
+        '''
+
+    def login(self):
 
 lagaWebsite = LagaAutomation()
 lagaWebsite.setup()
 lagaWebsite.Open_WebBrowser()
 lagaWebsite.Reg()
 lagaWebsite.email("tahsinanijum5199@gmail.com")
-lagaWebsite.name("Tahsin Adiba", "Nijhum", "123Nipu")
+lagaWebsite.name("Tahsin", "Nijhum", "123Nipu*")
 lagaWebsite.dob(5,"January", 2000)
 lagaWebsite.personal_info("Tahsin Adiba", "Nijhum", "Red.Dot", "Downey", "Dominant Dream", "South Gate", "California", "90280", "Information will be added later if needed" ,"+12022563030", "+8801629099448")
 
